eac48e0aca9e416c75fa54ed5b73a27379dcda30
James Colairo <jcolairo@spartaglobal.co>
2017-04-04 11:54:09 +0100

    commiting starter code which runs nodemon

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = require('./api/config/router');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var app = express();
+var PORT = process.env.PORT || 3000;
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/sg-project-3';
+
+mongoose.connect(MONGODB_URI, function (err) {
+  if (err) {
+    console.error('Could not connect to Mongo: err:', err);
+    process.exit(1);
+  }
+  console.log('Connected to database:', mongoose.connection.name);
+});
+
+app.use(function (req, res, next) {
+ // simple middleware logging
+  console.log(req.method, req.path);
+  next();
+});
+app.use(express.static('frontend'));
+app.use(express.static('node_modules'));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+// app.use(router);
+
+app.listen(PORT, function() {
+  console.log('App is running on port', PORT);
+});
+
+module.exports = app;
