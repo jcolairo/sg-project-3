cfe236f8b6aa19d40403611d4a0bc8aea0a60b93
James Colairo <jcolairo@spartaglobal.co>
2017-04-11 09:10:49 +0100

    basic template for centered google maps

--- frontend/js/controllers/crime.controller.js
+++ frontend/js/controllers/crime.controller.js
@@ -7,9 +7,33 @@ function CrimeController(CrimeFactory) {
     controller.crimeLat = '';
     controller.crimeLng = '';
     controller.crimeDate = '';
-  }
-  controller.getCrimeDetails = function (lat, lng) {
-    CrimeFactory.getAllCrimes(
+    controller.map = { center: { latitude: 51.45, longitude: -0.3}, zoom: 15};
+    controller.marker = {
+      id: 0,
+      coords: {
+        latitude: 51.45,
+        longitude: -0.3
+      },
+      options: { draggable: true },
+      events: {
+        dragend: function(marker, eventName, $log) {
+          $log.log('marker dragend');
+          var lat = marker.getPosition().lat();
+          var lng = marker.getPosition().lng();
+          $log.log(lat);
+          $log.log(lng);
+
+          controller.marker.options = {
+            draggable: true,
+            labelContent: 'lat: ' + controller.marker.coords.latitude + ' ' + 'lng: ' + controller.marker.coords.longitude,
+            labelAnchor: '100 0',
+            labelClass: 'marker-label'
+          };
+        }
+      }
+    };
+    controller.getCrimeDetails = function (lat, lng) {
+      CrimeFactory.getAllCrimes(
       controller.crimeName,
       lat,
       lng,
@@ -24,11 +48,10 @@ function CrimeController(CrimeFactory) {
         console.warn('Could not get crimes', error);
       }
     );
-  };
+    };
+  }
   init();
-
 }
-
 CrimeController.$inject = ['CrimeFactory', '$state'];
 
 angular
--- frontend/js/controllers/map.controller.js
+++ frontend/js/controllers/map.controller.js
@@ -4,7 +4,6 @@ function MapController(MapFactory) {
   function init() {
     console.log('MapController', controller);
     controller.placeName = '';
-    controller.map = { center: { latitude: 51.45, longitude: -0.3}, zoom: 15};
   }
 
   controller.selectLocation = function (place) {
