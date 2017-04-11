e0587ad02281513ee3472c899cb52a03ef9989d0
James Colairo <jcolairo@spartaglobal.co>
2017-04-11 11:23:09 +0100

    changed the zoom for google maps

--- frontend/js/controllers/crime.controller.js
+++ frontend/js/controllers/crime.controller.js
@@ -1,4 +1,4 @@
-function CrimeController(CrimeFactory, $state, $scope) {
+function CrimeController(CrimeFactory, $state) {
   var controller = this;
 
   function init() {
@@ -8,7 +8,7 @@ function CrimeController(CrimeFactory, $state, $scope) {
     controller.crimeLng = '';
     controller.crimeDate = '';
     controller.showMap = false;
-    controller.map = { center: { latitude: 51.45, longitude: -0.3}, zoom: 15};
+    controller.map = { center: { latitude: 51.45, longitude: -0.3}, zoom: 13};
 
     controller.getCrimeDetails = function (lat, lng) {
       controller.map.center = {
@@ -37,7 +37,7 @@ function CrimeController(CrimeFactory, $state, $scope) {
   }
   init();
 }
-CrimeController.$inject = ['CrimeFactory', '$state', '$scope'];
+CrimeController.$inject = ['CrimeFactory', '$state'];
 
 angular
   .module('PoliceApp')
